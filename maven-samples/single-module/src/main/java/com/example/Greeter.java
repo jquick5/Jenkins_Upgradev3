111eccbe63c85ab6dd454327045d8722a9b7c6e4
jquick5 <jay.quick79@gmail.com>
2022-01-25 11:26:02 -0800

    Updated index.jsp

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -12,15 +12,13 @@ public class Greeter {
 
   }
 
-  
-  //TODO: Add javadoc comment
+  // TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 
-  
-  public static void main(String args[]){
-    
-	  System.out.println("Hello from Main Method!!!");
+  public static void main(String args[]) {
+
+    System.out.println("Hello from Main Method!!!");
   }
 }
